e270f759753c8c6b08189e5a5d5486b2dc9fc4f2
Michael <Mlcorrieri@gmail.com>
2021-03-23 20:37:49 -0400

    commit tree bebc687da0f2781c27e6cf8ba88bb4dac5fc2bd7

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Add your code here
+// fetch(destinationURL, configurationObject);
+// const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (object) {
+//     console.log(object);
+//   });
+
+function submitData(userName, userEmail) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: `${userName}`,
+      email: `${userEmail}`,
+    }),
+  })
+    .then(function (res) {
+      return res.json;
+    })
+    .then(function (object) {
+      let body = document.querySelector("body");
+      body.append(object.id);
+    })
+    .catch(function (error) {
+      let body = document.querySelector("body");
+      body.append(error.message);
+    });
+}
